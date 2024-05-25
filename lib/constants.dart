afc13a652b007b1eb894a05eba69c684fbd1b977
Sathia Pang <pangvichetsatya1@gmail.com>
2024-05-25 18:15:57 +0700

    Done on screen login and register

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const green = Color.fromRGBO(41, 110, 72, 1);
+const greenAccent = Color(0xffE8F8EF);
+const black = Colors.black;
+const white = Colors.white;
+const textColor = Color(0xff616161);
+var borderfield = Colors.grey.shade300;
+const textfieldcolor = Color.fromARGB(255, 233, 231, 231);
+const red = Colors.red;
+const yellow = Colors.yellow;
+const boxcolor = Color.fromARGB(255, 243, 242, 242);
